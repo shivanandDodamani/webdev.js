4fbe0f43e86ea454844f62b80fcf55fbc0762393
shivanandDodamani <shivananddodamani635@gmail.com>
2026-03-17 23:36:14 +0530

    javascript

--- webdev/first.js
+++ webdev/first.js
@@ -1,10 +1,43 @@
-const profile = {
-    username: "shivanand Dodamani",
-    isfollow: false,
-    follow: 123,
-    followers: 123,
-    name:"@shivanand",
-    bio: "i am working in google as a software engineer",
-};
+// let a = 10;
+// let b = "20";
 
-console.log(profile)
+// // console.log("a =", a , "b =", b)
+// // console.log("a-- =", a--)
+// // console.log("a =", a)
+
+// // comparison operators
+// a = 10;
+// b = "10";
+// console.log("10 !== 10", a !== b)
+
+// logical operators
+// let a = 10;
+// let b = 20;
+// let cond1 = a < b;
+// let cond2 = a ===100;
+// console.log("cond1 !cond2", !(cond1 == cond2))
+
+//conditional statements
+
+// let a = 16;
+
+// // if(a > 18) {
+// //     console.log("you can vote")
+// // }
+
+// if(a < 18) {
+//     console.log("you cannot vote")
+// }
+
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+}
+
+if (mode === "light") {
+    color = "white";
+}
+
+console.log(color)
