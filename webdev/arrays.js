bccdfa7a1549bae2b6a74e08cf19690be6a65339
shivanandDodamani <shivananddodamani635@gmail.com>
2026-03-29 23:59:03 +0530

    javascript

--- webdev/arrays.js
+++ webdev/arrays.js
@@ -4,13 +4,23 @@
 //     console.log(names[i]);
 // }
 
-let marks = [85, 97, 44, 36, 76, 60];
-let sum = 0;
-for(let i = 0; i < marks.length; i++) {
-    sum+=marks[i];   
+// let marks = [85, 97, 44, 36, 76, 60];
+// let sum = 0;
+// for(let i = 0; i < marks.length; i++) {
+//     sum+=marks[i];   
+// }
+// let avg = sum/marks.length;
+// console.log("avarage = ", avg)
+
+let price =  [250, 625, 300, 900, 50];
+let i = 0;
+
+for ( let val of price) {
+    console.log("value of ", val);
+    let offer = val/10;
+    console.log("offer of ", val, " is ", offer);
+    i++;
 }
-let avg = sum/marks.length;
-console.log("avarage = ", avg)
 
 
 
